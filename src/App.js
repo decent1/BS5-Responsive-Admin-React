6d218917d9754aecd63f47e38c160f6f6395061a
Muhammad Shahid Khan Afridi <muhammadshahidkhanafridi@gmail.com>
2021-06-15 07:51:14 +0500

    Just setup static template to react
    Just setup static template to react and installed Bootstrap as well.
    Created separate component for Top Nav, Sidebar and Main page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/custom-css.css';
+import './assets/css/list-groups.css';
 import './App.css';
 import TopNavbar from './components/TopNavbar/TopNavbar';
 import MainPage from './components/MainPage/MainPage';
@@ -8,9 +11,14 @@ function App() {
   return (
     <>
       <TopNavbar />
-      
-      <LeftSidebar />
-      <MainPage />
+      <div id="wrapper" className="full-outer">
+        <div id="sidebar" className="sidebar-margin float-left shads padx-10-0">
+          <LeftSidebar />
+        </div>
+        <div id="main-page-wrapper" className="main-page pad-x-10">
+          <MainPage />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/LeftSidebar/LeftSidebar.js
+++ src/components/LeftSidebar/LeftSidebar.js
@@ -3,9 +3,104 @@ import React, { Component } from 'react';
 class LeftSidebar extends Component {
     render() {
         return (
-            <div>
-                <h1>This is sidebar</h1>
-            </div>
+            <>
+                <div>
+                    <div className="btn-sidebar-toggler" id="menu-toggle">
+                        <i className="bi bi-list" />
+                    </div>
+                    <div className="sidebar-inner">
+                        <div className="list-group list-group-root well">
+                            <a href="#item-1" className="list-group-item">
+                                <i className="glyphicon glyphicon-chevron-right" />Item 1
+                            </a>
+                            <div className="list-group " id="item-1">
+                                <a href="#item-1-1" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 1.1
+                                </a>
+                                <div className="list-group" id="item-1-1">
+                                    <a href="#" className="list-group-item">Item 1.1.1</a>
+                                    <a href="#" className="list-group-item">Item 1.1.2</a>
+                                    <a href="#" className="list-group-item">Item 1.1.3</a>
+                                </div>
+                                <a href="#item-1-2" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 1.2
+                                </a>
+                                <div className="list-group " id="item-1-2">
+                                    <a href="#" className="list-group-item">Item 1.2.1</a>
+                                    <a href="#" className="list-group-item">Item 1.2.2</a>
+                                    <a href="#" className="list-group-item">Item 1.2.3</a>
+                                </div>
+                                <a href="#item-1-3" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 1.3
+                                </a>
+                                <div className="list-group " id="item-1-3">
+                                    <a href="#" className="list-group-item">Item 1.3.1</a>
+                                    <a href="#" className="list-group-item">Item 1.3.2</a>
+                                    <a href="#" className="list-group-item">Item 1.3.3</a>
+                                </div>
+                            </div>
+                            <a href="#item-2" className="list-group-item">
+                                <i className="glyphicon glyphicon-chevron-right" />Item 2
+                            </a>
+                            <div className="list-group" id="item-2">
+                                <a href="#item-2-1" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 2.1
+                                </a>
+                                <div className="list-group" id="item-2-1">
+                                    <a href="#" className="list-group-item">Item 2.1.1</a>
+                                    <a href="#" className="list-group-item">Item 2.1.2</a>
+                                    <a href="#" className="list-group-item">Item 2.1.3</a>
+                                </div>
+                                <a href="#item-2-2" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 2.2
+                                </a>
+                                <div className="list-group" id="item-2-2">
+                                    <a href="#" className="list-group-item">Item 2.2.1</a>
+                                    <a href="#" className="list-group-item">Item 2.2.2</a>
+                                    <a href="#" className="list-group-item">Item 2.2.3</a>
+                                </div>
+                                <a href="#item-2-3" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 2.3
+                                </a>
+                                <div className="list-group" id="item-2-3">
+                                    <a href="#" className="list-group-item">Item 2.3.1</a>
+                                    <a href="#" className="list-group-item">Item 2.3.2</a>
+                                    <a href="#" className="list-group-item">Item 2.3.3</a>
+                                </div>
+                            </div>
+                            <a href="#item-3" className="list-group-item">
+                                <i className="glyphicon glyphicon-chevron-right" />Item 3
+                            </a>
+                            <div className="list-group" id="item-3">
+                                <a href="#item-3-1" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 3.1
+                                </a>
+                                <div className="list-group" id="item-3-1">
+                                    <a href="#" className="list-group-item">Item 3.1.1</a>
+                                    <a href="#" className="list-group-item">Item 3.1.2</a>
+                                    <a href="#" className="list-group-item">Item 3.1.3</a>
+                                </div>
+                                <a href="#item-3-2" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 3.2
+                                </a>
+                                <div className="list-group" id="item-3-2">
+                                    <a href="#" className="list-group-item">Item 3.2.1</a>
+                                    <a href="#" className="list-group-item">Item 3.2.2</a>
+                                    <a href="#" className="list-group-item">Item 3.2.3</a>
+                                </div>
+                                <a href="#item-3-3" className="list-group-item">
+                                    <i className="glyphicon glyphicon-chevron-right" />Item 3.3
+                                </a>
+                                <div className="list-group" id="item-3-3">
+                                    <a href="#" className="list-group-item">Item 3.3.1</a>
+                                    <a href="#" className="list-group-item">Item 3.3.2</a>
+                                    <a href="#" className="list-group-item">Item 3.3.3</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
